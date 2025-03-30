a8e2360468fd9e507914736fe5f85c9caca1f1ab
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-03-30 19:36:11 +0200

    fetch posts from API and display in PostTable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import PostTable from "./components/PostTable";
 
-function App() {
-  const [count, setCount] = useState(0);
+const api_endpoint = "http://localhost:3000/posts";
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  function fetchPosts() {
+    fetch(api_endpoint)
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.error("Error fetching posts:", err));
+  }
 
   return (
-    <>
-      <h1>Counter</h1>
-    </>
+    <div>
+      <h1>Posts Blog</h1>
+      <PostTable posts={posts} />
+    </div>
   );
 }
-
-export default App;
